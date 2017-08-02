056b1a935a54d36c865bbc6a8d95c7f1c11c1c70
kjirou <sorenariblog@gmail.com>
2017-08-03 02:35:29 +0900

    babel is unnecessary

---
+++ index.js
@@ -0,0 +1,6 @@
+const {h, render} = require('ink');
+
+const App = require('./lib/App');
+
+
+render(h(App));
--- src/index.js
+++
@@ -1,3 +0,0 @@
-const {x, y, ...rest} = {x: 1, y: 2, z: 3, w: 4};
-
-console.log(rest);
