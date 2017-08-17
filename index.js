0afadf47a12e075b8a39e88f9c76bcc69aec20b7
kjirou <sorenariblog@gmail.com>
2017-08-17 22:39:22 +0900

    Investigate keypress event

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const App = require('./lib/App');
 
 
 render(h(App));
+
+// keypress
+//   - Ctrl+C と Escape による終了は ink/index.js で定義されている
+//     - これを直接 OFF する方法はなさそうなので、render 後に EventEmitter の作法で初期化などを行う必要がある
+//     - stdin/stdout は render の options で渡せる
+process.stdin.on('keypress', (ch, key) => {
+  // do stuff
+});
