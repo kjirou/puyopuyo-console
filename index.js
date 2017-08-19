c2bc519d458cf1456a8a77273f0f4690463c95b3
kjirou <sorenariblog@gmail.com>
2017-08-19 14:53:57 +0900

    Add a comment

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ render(h(App, {store}));
 //   - Ctrl+C と Escape による終了は ink/index.js で定義されている
 //     - これを直接 OFF する方法はなさそうなので、render 後に EventEmitter の作法で初期化などを行う必要がある
 //     - stdin/stdout は render の options で渡せる
+//   - 押しっぱなしでも OS のリピート間隔に従って連打扱いになる
 process.stdin.on('keypress', (ch, key) => {
   // do stuff
 });
