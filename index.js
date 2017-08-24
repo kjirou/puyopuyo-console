09c24c5bf64f734560a9fd36eced329035384480
kjirou <sorenariblog@gmail.com>
2017-08-24 21:23:04 +0900

    Make to operate falling puyos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {h, render} = require('ink');
 
 const Store = require('./lib/Store');
 const App = require('./lib/components/App');
+const {PAD_OPERATIONS} = require('./lib/constants');
 
 
 const store = new Store();
@@ -12,7 +13,13 @@ const store = new Store();
 //     - stdin/stdout は render の options で渡せる
 //   - 押しっぱなしでも OS のリピート間隔に従って連打扱いになる
 process.stdin.on('keypress', (ch, key) => {
-  // do stuff
+  if (key.name === 'right') {
+    store.receivePadOperation(PAD_OPERATIONS.RIGHT);
+  } else if (key.name === 'left') {
+    store.receivePadOperation(PAD_OPERATIONS.LEFT);
+  } else if (key.name === 'down') {
+    store.receivePadOperation(PAD_OPERATIONS.DOWN);
+  }
 });
 
 render(h(App, {store}));
